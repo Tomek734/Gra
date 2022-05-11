b247d058e0cb139de7eb395a1709823ba3fd2910
Tomek734 <103137417+Tomek734@users.noreply.github.com>
2022-05-11 20:02:52 +0200

    Add files via upload

--- Gra/POSTAĆ/Bohater.py
+++ Gra/POSTAĆ/Bohater.py
@@ -43,7 +43,7 @@ class Bohater(Postać):
                 root.mainloop()
                 break
             else:
-                print('Nie ma takiej kategori')
+                print('Nie ma takiej kategorii')
     def walka(self, Dres):
         print(f'Spotykasz dresa {Dres.name}')
         print('Możesz z nim waliczyć-a lub uciekać-b')
@@ -51,10 +51,10 @@ class Bohater(Postać):
         if inp=='a':
             if self.p>Dres.p:
                 self.p+=20
-                print(f"Wygrywasz walkę{self.p}")
+                print(f"Wygrywasz walkę {self.p}")
             else:
                 self.p-= randint(20,30)
-                print(f"Dres okazał się być śilniejszy{self.p}")
+                print(f"Dres okazał się być śilniejszy {self.p}")
         elif inp=='b':
             inp=choice(['v','n','g'])
             if inp=='v' or 'g':
@@ -73,7 +73,7 @@ class Bohater(Postać):
     def kartkowka(self):
         if 'ściąga' in self.equipment:
             print('Masz ściągę')
-            inp=input('Czy chcesz ją wykorzystać?')
+            inp=input('Czy chcesz ją wykorzystać?\t')
             if inp=='tak':
                 self.equipment.remove('ściąga')
                 inp2=choice(['d','l','k'])
@@ -97,57 +97,103 @@ class Bohater(Postać):
                 inp2=randint(20,40)
                 print(f'Tracisz {self.p} punktów')
     def odpowiedz(self):
-        print('Spróbuj porzmawiać na inny teamt-a')
-        print('Odpowiadaj na pytania-b')
-        inp=input('Co wybierasz?')
-        if inp=='a':
-            inp=choice(['d','l'])
-            if inp=='d':
-                print('Udało Ci się zagadać nauczyciela!!')
-                print('Nie musiałeś odpowiadać w nagrodę otrymujesz 45 punktów!!')
-                self.p+=45
-            elif inp=='l':
-                print('Nauczyciel zorientowa się, że nie znasz odpowiedzi na jego pytania')
-                print('Tracisz 45 punktów!!')
-                self.p-=45
-        elif inp=='b':
-            inp=choice(['d','l'])
-            if inp=='d':
-                print('Znałeś odpowiedzi na wszystkie pytania')
-                print('W nagrodę otzymujesz 50 punktów')
-                self.p+=50
-            elif inp=='l':
-                print('Nie przygotowałeś się')
-                print('Tracisz 40 punktów!!')
-                self.p-=40
+        while True:
+            print('Spróbuj porzmawiać na inny teamt-a')
+            print('Odpowiadaj na pytania-b')
+            inp=input('Co wybierasz?\t')
+            if inp=='a':
+                inp=choice(['d','l'])
+                if inp=='d':
+                    print('Udało Ci się zagadać nauczyciela!!')
+                    print('Nie musiałeś odpowiadać w nagrodę otrymujesz 45 punktów!!')
+                    self.p+=45
+                    break
+                elif inp=='l':
+                    print('Nauczyciel zorientowa się, że nie znasz odpowiedzi na jego pytania')
+                    print('Tracisz 45 punktów!!')
+                    self.p-=45
+                    break
+            elif inp=='b':
+                inp=choice(['d','l'])
+                if inp=='d':
+                    print('Znałeś odpowiedzi na wszystkie pytania')
+                    print('W nagrodę otzymujesz 50 punktów')
+                    self.p+=50
+                    break
+                elif inp=='l':
+                    print('Nieprzygotowałeś się')
+                    print('Tracisz 40 punktów!!')
+                    self.p-=40
+                    break
+            else:
+                print('Wybierz jedną z opcji')
     def sprawdzian(self):
-        print('Możesz zrobić trudny sprawdzian - a lub łatwy - b')
-        inp=input('Co wybierasz?')
-        if inp=='a':
-            inp=choice(['d','l','z'])
-            if inp=='d' or 'z':
-                print('Niestety ucznowie nipodołali tak trudnym zadaniom')
-                inp=randint(60,100)
-                self.p-=inp
-                print(f'Tracisz {inp} punktów')
-            elif inp=='l':
-                print('Uczniowie zdołali odpowiedzieć na pytania')
-                inp=randint(70,130)
-                self.p+=inp
-                print(f'W nagrodę otrzymujesz {inp} punktów')
-        elif inp=='b':
-            inp=choice(['d','l','z'])
-            if inp=='d' or 'z':
-                print('Dzięki łatwemu sprawdzianowi wszystkim udaje się zaliczyć')
-                inp=randint(40,75)
-                self.p+=inp
-                print(f'W nagrodę otrzymujesz {inp} punktów')
-            elif inp=='l':
-                print('Uczniowie nie odpowiedzili na tak proste pytania')
-                inp=randint(30,60)
-                self.p-=inp
-                print(f'Tracisz {inp} punktów')
-    def odp(self):
+        while True:
+            print('Możesz zrobić trudny sprawdzian - a lub łatwy - b')
+            inp=input('Co wybierasz?\t')
+            if inp=='a':
+                inp=choice(['d','l','z'])
+                if inp=='d' or 'z':
+                    print('Niestety ucznowie nipodołali tak trudnym zadaniom')
+                    inp=randint(60,100)
+                    self.p-=inp
+                    print(f'Tracisz {inp} punktów')
+                    break
+                elif inp=='l':
+                    print('Uczniowie zdołali odpowiedzieć na pytania')
+                    inp=randint(70,130)
+                    self.p+=inp
+                    print(f'W nagrodę otrzymujesz {inp} punktów')
+                    break
+            elif inp=='b':
+                inp=choice(['d','l','z'])
+                if inp=='d' or 'z':
+                    print('Dzięki łatwemu sprawdzianowi wszystkim udaje się zaliczyć')
+                    inp=randint(40,75)
+                    self.p+=inp
+                    print(f'W nagrodę otrzymujesz {inp} punktów')
+                    break
+                elif inp=='l':
+                    print('Uczniowie nie odpowiedzili na tak proste pytania')
+                    inp=randint(30,60)
+                    self.p-=inp
+                    print(f'Tracisz {inp} punktów')
+                    break
+            else:
+                print('Wybierz jedną z opcji')
+    def pytanie(self):
         print('Bierzesz ucznia do odpowiedzi')
-        if i==0
+        while True:
+            print('Zadajesz mu trudne pytania-a lub łatwe-b')
+            inp=input('?\t')
+            if inp=='a':
+                inp=choice(['d','l','z'])
+                if inp=='d' or 'z':
+                    print('Niestety uczeń nie odpowiedzaiał na twoje pytania')
+                    inp=randint(60,100)
+                    self.p-=inp
+                    print(f'Tracisz {inp} punktów')
+                    break
+                elif inp=='l':
+                    print('Uczń udzielił odpowiedzi na twoje trudne pytania')
+                    inp=randint(70,130)
+                    self.p+=inp
+                    print(f'W nagrodę otrzymujesz {inp} punktów')
+                    break
+            elif inp=='b':
+                inp=choice(['d','l','z'])
+                if inp=='d' or 'z':
+                    print('Dzięki łatwym pytaniom uczeń zdołał odpowiedzieć')
+                    inp=randint(40,75)
+                    self.p+=inp
+                    print(f'W nagrodę otrzymujesz {inp} punktów')
+                    break
+                elif inp=='l':
+                    print('Uczeń nie odpowiedział na ani jedno pytanie')
+                    inp=randint(30,60)
+                    self.p-=inp
+                    print(f'Tracisz {inp} punktów')
+                    break
+            else:
+                print('Wybierz jedną z opcji')
 
